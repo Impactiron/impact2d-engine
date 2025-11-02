d2962eb52744d11ce1a6f513ccdf53b10e3e0784
Impactiron <rothmich84@gmail.com>
2025-11-02 12:55:26 +0100

    Add files via upload

--- engine/factory.js
+++ engine/factory.js
@@ -1,4 +1,3 @@
-
 class Crate {
   constructor(p = {}) {
     this.type = 'crate';
@@ -15,16 +14,15 @@ const _prefabs = new Map();
 
 export const factory = {
   register(name, ctor) {
-    if (!name || (!ctor) || !(['function','object'].includes(typeof ctor))) {
+    if (!name || !ctor || !(['function', 'object'].includes(typeof ctor))) {
       console.warn('[factory.register] invalid registration for', name);
       return;
     }
     _prefabs.set(name, ctor);
-  }
-    _prefabs.set(name, ctor);
   },
+
+  // Supports: spawn(name, props) and spawn(name, game, props)
   spawn(name, a = null, b = {}) {
-    // supports spawn(name, props) and spawn(name, game, props)
     let game = null, props = {};
     if (a && typeof a === 'object' && !('addChild' in a) && !('addTo' in a)) {
       props = a || {};
@@ -45,26 +43,16 @@ export const factory = {
       console.warn('[factory.spawn] attach skipped:', e);
     }
     return entity;
-  }`);
-      return null;
-    }
-    const entity = new Ctor(props);
-    try {
-      if (entity && typeof entity.addTo === 'function' && game) entity.addTo(game);
-      else if (game && typeof game.addChild === 'function') game.addChild(entity);
-    } catch (e) {
-      console.warn('[factory.spawn] attach skipped:', e);
-    }
-    return entity;
   },
+
   clear() { _prefabs.clear(); },
   list() { return Array.from(_prefabs.keys()); }
 };
 
 export function registerDefaultPrefabs() {
   try { factory.register('crate', Crate); } catch (e) { /* ignore if double */ }
-  class Gem { constructor(p={}){ this.x=p.x|0; this.y=p.y|0; this.type='gem'; this.size=p.size||16; } }
-  class Bot { constructor(p={}){ this.x=p.x|0; this.y=p.y|0; this.type='bot'; this.size=p.size||16; this.speed=p.speed||1; } }
+  class Gem { constructor(p = {}) { this.x = p.x|0; this.y = p.y|0; this.type = 'gem'; this.size = p.size || 16; } }
+  class Bot { constructor(p = {}) { this.x = p.x|0; this.y = p.y|0; this.type = 'bot'; this.size = p.size || 16; this.speed = p.speed || 1; } }
   factory.register('gem', Gem);
   factory.register('bot', Bot);
 }
